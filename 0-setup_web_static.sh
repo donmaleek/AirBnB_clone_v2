05c4dd8fc7be1debd4b4f22580ec0048d99204be
donmaleek <bamboloshari@gmail.com>
2024-04-07 06:29:55 -0700

    version 2 fork

---
+++ 0-setup_web_static.sh
@@ -0,0 +1,51 @@
+#!/usr/bin/env bash
+# Sets up a web server for deployment of web_static.
+
+apt-get update
+apt-get install -y nginx
+
+sudo mkdir -p /data/
+sudo mkdir -p /data/web_static/
+sudo mkdir -p /data/web_static/releases/
+sudo mkdir -p /data/web_static/shared/
+sudo mkdir -p /data/web_static/releases/test/
+
+sudo echo "<html>
+  <head>
+  <title>Air BnB Clone</title>
+  </head>
+  <body>
+    <h2>Holberton School<h2>
+  </body>
+</html>" | sudo tee /data/web_static/releases/test/index.html
+
+sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
+
+sudo chown -R ubuntu /data/
+sudo chgrp -R ubuntu /data/
+
+printf %s "server {
+    listen 80 default_server;
+    listen [::]:80 default_server;
+    add_header X-Served-By $HOSTNAME;
+    root   /var/www/html;
+    index  index.html index.htm;
+
+    location /hbnb_static {
+        alias /data/web_static/current;
+        index index.html index.htm;
+    }
+
+    location /redirect_me {
+        return 301 http://cuberule.com/;
+    }
+
+    error_page 404 /404.html;
+    location /404 {
+      root /var/www/html;
+      internal;
+    }
+}" > /etc/nginx/sites-available/default
+
+sudo service nginx restart
+
